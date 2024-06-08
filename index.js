504fbda45fabaccb38caddc892be4068083095cf
Yuvraj <singhyuvraj0506@gmail.com>
2024-06-09 02:02:31 +0530

    SSR to check

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const { Server } = require('socket.io');
 const { createServer } = require('http');
-require("dotenv").config()
+const path = require('path');
+require("dotenv").config();
 
 const app = express();
 const server = createServer(app);
@@ -24,54 +24,58 @@ const io = new Server(server, {
     }
 });
 
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    return res.send("Hwllo Guys")
-})
+// Serve static files from the Vite build directory
+app.use(express.static(path.join(__dirname, 'dist')));
 
-let usersMap = []
+// Serve the Vite build for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+let usersMap = [];
 
 io.on('connection', (socket) => {
-    console.log('a user connected' , socket?.id);
-    
-    socket.on("userConnect",({username})=>{
+    console.log('a user connected', socket.id);
+
+    socket.on("userConnect", ({ username }) => {
         usersMap.push({
             socket: socket.id,
             username: username
-        })
-        socket.broadcast.emit("new-user-joined",socket?.id)
-        console.log("Usercount -----", usersMap?.length)
-    })
+        });
+        socket.broadcast.emit("new-user-joined", socket.id);
+        console.log("Usercount -----", usersMap.length);
+    });
 
-    socket.on("offerSent",data => {
-        const offerReciever = usersMap?.find((e)=>(e.socket === data?.reciever))
-        if(offerReciever){
-            socket.to(offerReciever?.socket).emit("RecieveOffer",data)
+    socket.on("offerSent", data => {
+        const offerReceiver = usersMap.find(e => e.socket === data.reciever);
+        if (offerReceiver) {
+            socket.to(offerReceiver.socket).emit("RecieveOffer", data);
         }
-    })
+    });
 
-    socket.on("answerCreated",data => {
-        const answerReciever = usersMap?.find((e)=>(e.socket === data?.reciever))
-        if(answerReciever){
-            socket.to(answerReciever?.socket).emit("RecieveAnswer",data?.answer)
+    socket.on("answerCreated", data => {
+        const answerReceiver = usersMap.find(e => e.socket === data.reciever);
+        if (answerReceiver) {
+            socket.to(answerReceiver.socket).emit("RecieveAnswer", data.answer);
         }
-    })
+    });
 
-    socket.on("sendIceCandidate",data => {
-        const reciever = usersMap?.find((e)=>(e.socket === data?.reciever))
-        if(reciever){
-            socket.to(reciever?.socket).emit("SetPeerIce",data?.iceCandidateData)
+    socket.on("sendIceCandidate", data => {
+        const receiver = usersMap.find(e => e.socket === data.reciever);
+        if (receiver) {
+            socket.to(receiver.socket).emit("SetPeerIce", data.iceCandidateData);
         }
-    })
+    });
 
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected!!!!")
-        usersMap = usersMap?.filter((e)=>(e.socket !== socket?.id))
-    })
-  });
+    socket.on("disconnect", () => {
+        console.log("User Disconnected!!!!");
+        usersMap = usersMap.filter(e => e.socket !== socket.id);
+    });
+});
 
 server.listen(5000, () => {
     console.log('server running at http://localhost:5000');
-  });
+});
